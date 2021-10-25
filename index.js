88f1f888961f6b9b7927078c1f2610eb5734ce88
krishnkant <krishnkantpachori50@gmail.com>
2021-10-25 15:55:44 +0530

    okk

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // node server which will handel socket io connections
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('index.html').pipe(res)
+})
+server.listen(process.env.PORT || 8000)
+
 const io=require('socket.io')(process.env.PORT||8000)
 
 const users={};
